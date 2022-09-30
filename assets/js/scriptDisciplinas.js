f6e540da2d0b285436e2728df6602525d3eb8ed0
Igor Parizi <parizi57@gmail.com>
2022-09-30 19:29:19 -0300

    Mudanças script disciplinas

--- assets/js/scriptDisciplinas.js
+++ assets/js/scriptDisciplinas.js
@@ -1,7 +1,38 @@
 let arrayDisciplinas = [];
 
-function cadastraDisciplina(){
+function cadastrarDisciplina(){
     let disciplinas = {
-        
+        Sigla: document.getElementById("sigla").value,
+        NomeDisciplina: document.getElementById("nomed").value,
+        Ementa: document.getElementById("ementa").value,
+        LivrosBibliografia: document.getElementById("livros_bibliografia").value,
+        NumeroCreditos: document.getElementById("numeroCreditos").value,
+        CargaHoraria: document.getElementById("cargaHoraria").value,
+    };
+
+    function verificarCadastro(VetorDisciplina, Sigla) {
+        for (let i = 0; i < VetorDisciplina.length; i++) {
+          if (VetorDisciplina[i].Sigla == Sigla) {
+            return i;
+          }
+        }
+        return -1;
+      }
+
+    if (Object.values(disciplinas).includes(""))
+        return alert("Preencha todos os campos para concluir o cadastro!");
+
+    let cadastro = verificarCadastro(
+        arrayDisciplinas,
+        disciplinas.Sigla
+        );
+
+    if (cadastro == -1) {
+        arrayProfessores.push(professores);
+        console.log(arrayProfessores);
+        return alert("Cadastro realizado com sucesso!");
+    } else {
+        return alert("Disciplina já está cadastrada no sistema!");
     }
 }
+
