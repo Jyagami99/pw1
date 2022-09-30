e5fbf349fe462e94d3cb67b78b5d910e29b834e7
João Marcelo Simão de Castro <jm.castro@usp.br>
2022-09-30 01:43:42 -0300

    feat: add function to sign a professor

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let arrayProfessores = [];
- 
+
 function cadastroProfessor() {
   let professores = {
     RegistroFuncional: document.getElementById("registrof").value,
@@ -9,68 +9,152 @@ function cadastroProfessor() {
     AreaPesquisa: document.getElementById("areap").value,
     Universidade: document.getElementById("universi").value,
     Email: document.getElementById("email").value,
-    Telefone: document.getElementById("telefone").value
+    Telefone: document.getElementById("telefone").value,
   };
 
-  Cadastro = verificarCadastro(arrayProfessores, professores.RegistroFuncional);
-  if(Cadastro == -1){
-    arrayProfessores.push(professores);
-    alert("Professor cadastrado no sistema!");
-  }
-  else{
-    alert("Professor já cadastrado no sistema!")
+  function verificarCadastro(VetorProfessor, RegistroFuncional) {
+    for (let i = 0; i < VetorProfessor.length; i++) {
+      if (VetorProfessor[i].RegistroFuncional == RegistroFuncional) {
+        return i;
+      }
+    }
+    return -1;
   }
 
   if (Object.values(professores).includes(""))
     return alert("Preencha todos os campos para concluir o cadastro!");
-}
 
-function verificarCadastro(VetorProfessor,RegistroFuncional)
-{
-    let i = 0;
-    for(i=0;i<VetorProfessor.length;i++){
-        if (VetorProfessor[i].RegistroFuncional == RegistroFuncional)
-        {
-            return i;
-        }
-    }
-    return -1;
+  let cadastro = verificarCadastro(
+    arrayProfessores,
+    professores.RegistroFuncional
+  );
+
+  if (cadastro == -1) {
+    arrayProfessores.push(professores);
+    console.log(arrayProfessores);
+    return alert("Cadastro realizado com sucesso!");
+  } else {
+    return alert("Professor já está cadastrado no sistema!");
+  }
 }
 
 function mostraProfessor() {
-  let profCadastrado = "<table><tr><th>Registro Funcional</td><th>Nome</td><th>Data de nascimento</td><th>Sexo</td><th>Área de Pesquisa</td><th>Universidade de Formação</td><th>Email(s)</td><th>Telefone(s)</td></tr>";
-  let i = 0;
+  if (arrayProfessores.length > 0) {
+    console.log(arrayProfessores);
+    let formulario = document.querySelector(".formularioCadastro");
+    formulario.classList.toggle("d-none");
 
-  if(arrayProfessores.length > 0){
-    for(i=0; i < arrayProfessores.length; i++){
-      profCadastrado += "<tr><td>" + arrayProfessores[i].RegistroFuncional + "<tr><td>" + arrayProfessores[i].NomeCompleto + "<tr><td>" + arrayProfessores[i].DataDeNascimento + "<tr><td>" + arrayProfessores[i].Sexo + "<tr><td>" + arrayProfessores[i].AreaPesquisa + "<tr><td>" + arrayProfessores[i].Universidade + "<tr><td>" + arrayProfessores[i].Email + "<tr><td>" + arrayProfessores[i].Telefone + "<tr><td>";
-      profCadastrado += "</table>";
-
-      document.getElementById("tabelaLista").innerHTML = profCadastrado;
-    }
+    let listaDeProfessores = "";
+    listaDeProfessores += ` 
+    <p>
+      Registro Funcional: ${arrayProfessores[i].RegistroFuncional} 
+      <br>
+      Nome Completo:  ${arrayProfessores[i].NomeCompleto}
+      <br>
+      Data de Nascimento: ${arrayProfessores[i].DataDeNascimento}
+      <br>
+      Sexo: ${arrayProfessores[i].Sexo} 
+      <br>
+      Área de Pesquisa: ${arrayProfessores[i].AreaPesquisa} 
+      <br>
+      Universidade: ${arrayProfessores[i].Universidade} 
+      <br>
+      Email:  ${arrayProfessores[i].Email} 
+      <br>
+      Telefone: ${arrayProfessores[i].Telefone}
+    </p>`;
+    document.querySelector(".conteudo").innerHTML = listaDeProfessores;
+  } else {
+    console.log(arrayProfessores);
+    alert("Não há professores cadastrados!");
   }
-  else{
-    document.getElementById("tabelaLista").innerHTML = "Não existe professores cadastrados no sistema!";
-  };
+
+  // let profCadastrado = `
+  // <table>
+  //   <tr>
+  //     <th>Registro Funcional
+  //     </td>
+  //     <th>Nome
+  //     </td>
+  //     <th>Data de nascimento
+  //     </td>
+  //     <th>Sexo
+  //     </td>
+  //     <th>Área de Pesquisa
+  //     </td>
+  //     <th>Universidade de Formação
+  //     </td>
+  //     <th>Email(s)
+  //     </td>
+  //     <th>Telefone(s)
+  //     </td>
+  //   </tr>`;
+  // if (arrayProfessores.length > 0) {
+  //   for (let i = 0; i < arrayProfessores.length; i++) {
+  //     profCadastrado +=
+  //       "<tr><td>" +
+  //       arrayProfessores[i].RegistroFuncional +
+  //       "<tr><td>" +
+  //       arrayProfessores[i].NomeCompleto +
+  //       "<tr><td>" +
+  //       arrayProfessores[i].DataDeNascimento +
+  //       "<tr><td>" +
+  //       arrayProfessores[i].Sexo +
+  //       "<tr><td>" +
+  //       arrayProfessores[i].AreaPesquisa +
+  //       "<tr><td>" +
+  //       arrayProfessores[i].Universidade +
+  //       "<tr><td>" +
+  //       arrayProfessores[i].Email +
+  //       "<tr><td>" +
+  //       arrayProfessores[i].Telefone +
+  //       "<tr><td>";
+  //     profCadastrado += "</table>";
+  //     document.getElementById("container").innerHTML = profCadastrado;
+  //     console.log(profCadastrado);
+  //   }
+  // } else {
+  //   console.log(profCadastrado);
+  //   document.getElementById("container").innerHTML =
+  //     "Não existe professores cadastrados no sistema!";
+  // }
 }
 
 function listarEspecifico() {
   let rf = document.getElementById("listarProfessor").value;
-  let profCadastrado = "<table><tr><th>Registro Funcional</td><th>Nome</td><th>Data de nascimento</td><th>Sexo</td><th>Área de Pesquisa</td><th>Universidade de Formação</td><th>Email(s)</td><th>Telefone(s)</td></tr>";
+  let profCadastrado =
+    "<table><tr><th>Registro Funcional</td><th>Nome</td><th>Data de nascimento</td><th>Sexo</td><th>Área de Pesquisa</td><th>Universidade de Formação</td><th>Email(s)</td><th>Telefone(s)</td></tr>";
 
-  if(arrayProfessores.length > 0){
+  if (arrayProfessores.length > 0) {
     cadastro = verificarCadastro(arrayProfessores, rf);
-    if(cadastro != - 1){
-      profCadastrado += "<tr><td>" + arrayProfessores[i].RegistroFuncional + "<tr><td>" + arrayProfessores[i].NomeCompleto + "<tr><td>" + arrayProfessores[i].DataDeNascimento + "<tr><td>" + arrayProfessores[i].Sexo + "<tr><td>" + arrayProfessores[i].AreaPesquisa + "<tr><td>" + arrayProfessores[i].Universidade + "<tr><td>" + arrayProfessores[i].Email + "<tr><td>" + arrayProfessores[i].Telefone + "<tr><td>";
+    if (cadastro != -1) {
+      profCadastrado +=
+        "<tr><td>" +
+        arrayProfessores[i].RegistroFuncional +
+        "<tr><td>" +
+        arrayProfessores[i].NomeCompleto +
+        "<tr><td>" +
+        arrayProfessores[i].DataDeNascimento +
+        "<tr><td>" +
+        arrayProfessores[i].Sexo +
+        "<tr><td>" +
+        arrayProfessores[i].AreaPesquisa +
+        "<tr><td>" +
+        arrayProfessores[i].Universidade +
+        "<tr><td>" +
+        arrayProfessores[i].Email +
+        "<tr><td>" +
+        arrayProfessores[i].Telefone +
+        "<tr><td>";
       profCadastrado += "</table>";
 
       document.getElementById("tabelaEspecifico").innerHTML = profCadastrado;
+    } else {
+      document.getElementById("tabelaEspecifico").innerHTML =
+        "Professor não encontrado no sistema!";
     }
-    else{
-      document.getElementById("tabelaEspecifico").innerHTML = "Professor não encontrado no sistema!";
-    }
-  }
-  else{
-    document.getElementById("tabelaEspecifico").innerHTML = "Não existe professores cadastrados no sistema!";
+  } else {
+    document.getElementById("tabelaEspecifico").innerHTML =
+      "Não existe professores cadastrados no sistema!";
   }
 }
