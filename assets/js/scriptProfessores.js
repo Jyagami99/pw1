d1d4596b153b6e98ce859e38400f3d1e03f55e57
João Marcelo Simão de Castro <jm.castro@usp.br>
2022-09-30 23:51:30 -0300

    feat: add function alteraDado

--- assets/js/scriptProfessores.js
+++ assets/js/scriptProfessores.js
@@ -39,9 +39,6 @@ function cadastroProfessor() {
 
 function mostraProfessor() {
   if (arrayProfessores.length > 0) {
-    let formulario = document.querySelector(".formularioCadastro");
-    formulario.classList.toggle(".d-none");
-
     let listaDeProfessores = "";
 
     for (let i = 0; i < arrayProfessores.length; i++) {
@@ -103,8 +100,6 @@ function pesquisarPorRF() {
   } else {
     alert("Insira um RF válido!");
   }
-
-  // console.log(registroFuncional);
 }
 
 function listarEspecifico() {
@@ -196,7 +191,6 @@ function excluiDado() {
   let registroFuncional = document.getElementById(
     "registroFuncionalEspecifico"
   ).value;
-  let conteudo = document.querySelector(".conteudo");
 
   if (registroFuncional.length !== 0) {
     for (let i = 0; i < arrayProfessores.length; i++) {
@@ -205,6 +199,7 @@ function excluiDado() {
         alert(
           `O professor com RF ${registroFuncional} foi deletado do sistema!`
         );
+        break;
       }
     }
   } else {
@@ -224,6 +219,138 @@ function excluiEspecifico() {
       required
     />
     <button onclick="excluiDado()">Pesquisar</button>
+  </div>
+  `;
+}
+
+function atualizaCadastro() {
+  let dados = {
+    NomeCompleto: document.getElementById("nomec").value,
+    DataDeNascimento: document.getElementById("datanasc").value,
+    Sexo: document.getElementById("sexo").value,
+    AreaPesquisa: document.getElementById("areap").value,
+    Universidade: document.getElementById("universi").value,
+    Email: document.getElementById("email").value,
+    Telefone: document.getElementById("telefone").value,
+  };
+  
+  let registroFuncional = document.getElementById(
+    "registroFuncionalEspecifico"
+  ).value;
+
+  for (let i = 0; i < arrayProfessores.length; i++) {
+    if (arrayProfessores[i][0] === registroFuncional) {
+      arrayProfessores[i].splice(1, 1, dados.NomeCompleto);
+      arrayProfessores[i].splice(2, 1, dados.DataDeNascimento);
+      arrayProfessores[i].splice(3, 1, dados.Sexo);
+      arrayProfessores[i].splice(4, 1, dados.AreaPesquisa);
+      arrayProfessores[i].splice(5, 1, dados.Universidade);
+      arrayProfessores[i].splice(6, 1, dados.Email);
+      arrayProfessores[i].splice(7, 1, dados.Telefone);
+      break;
+    }
+  }
+}
+
+function exibeDadosProfessor() {
+  let conteudo = document.querySelector(".conteudo");
+  conteudo.innerHTML = `
+  <form class="formularioCadastro" name="searchfield" method="put">
+    <input
+      type="text"
+      id="registrof"
+      name="registrof"
+      placeholder="Registro Funcional"
+      disabled
+      required
+    />  
+    <input
+      type="text"
+      id="nomec"
+      name="nomec"
+      placeholder="Nome completo"
+      required
+    />
+    <input
+      type="date"
+      id="datanasc"
+      name="datanasc"
+      placeholder="Data de nascimento"
+      required
+    />
+    <input
+      type="text"
+      id="sexo"
+      name="sexo"
+      placeholder="Sexo"
+      required
+    />
+    <input
+      type="text"
+      id="areap"
+      name="areap"
+      placeholder="Area de pesquisa"
+      required
+    />
+    <input
+      type="text"
+      id="universi"
+      name="universi"
+      placeholder="Universidade"
+      required
+    />
+    <input
+      type="email"
+      id="email"
+      name="email"
+      placeholder="Email"
+      required
+    />
+    <input
+      type="tel"
+      id="telefone"
+      name="telefone"
+      minlength="10"
+      maxlength="11"
+      placeholder="Telefone"
+      required
+    />
+  </form>
+  <button id="botao" class="botao" onclick="atualizaCadastro()">
+    Altera dado do Professor
+  </button>
+  `;
+}
+
+function alteraDado() {
+  let registroFuncional = document.getElementById(
+    "registroFuncionalEspecifico"
+  ).value;
+
+  if (registroFuncional.length !== 0) {
+    for (let i = 0; i < arrayProfessores.length; i++) {
+      if (arrayProfessores[i][0] === registroFuncional) {
+        exibeDadosProfessor();
+        break;
+      }
+    }
+  } else {
+    alert("Insira um RF válido!");
+  }
+}
+
+function alteraEspecifico() {
+  let conteudo = document.querySelector(".conteudo");
+  conteudo.innerHTML = `
+  <div class="pesquisaEspecifica">
+    <input
+      type="text"
+      class="registroFuncionalEspecifico"
+      id="registroFuncionalEspecifico"
+      placeholder="Digite o RF que deseja alterar"
+      required
+    />
+    <button onclick="alteraDado()">Pesquisar</button>
   </div>
   `;
 }
